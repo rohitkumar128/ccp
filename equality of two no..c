3827f88111451a643cc7937e370b8c69b41d994b
rohitkumar128 <83941130+rohitkumar128@users.noreply.github.com>
2021-07-16 16:58:47 +0530

    Add files via upload

---
+++ equality of two no..c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int m,n;
+printf("enter the values for m and n \n");
+scanf("%d %d", &m,&n);
+if(m==n)
+printf("m and n are equal\n");
+else
+printf("m and are not equal\n");
+return 0;
+}
